9628f2bc01bd71dc33280bf6ac2c28509e440ab0
ToruNiina <niina.toru.68u@gmail.com>
2017-06-05 00:05:20 +0900

    split and improve impl of xorshift

---
+++ pseudo/xorshift.hpp
@@ -0,0 +1,262 @@
+#ifndef PSEUDO_XORSHIFT
+#define PSEUDO_XORSHIFT
+
+/* This is based on the article "Xorshift RNGs" by George Marsaglia (2003)    *
+ * Journal of Statistical Software Vol. 8 (Issue  14). All parameters used    *
+ * here in default are taken from the article.                                */
+
+#if __cplusplus < 201103L
+#error "pseudo/xorshift is for c++11 random"
+#endif
+
+#include <type_traits>
+#include <limits>
+#include <cstdint>
+
+namespace psd
+{
+
+enum class xorshift_period_tag
+{
+    p32,
+    p64,
+    p96,
+    p128,
+    p160,
+};
+
+template<typename uintT, uintT a, uintT b, uintT c, xorshift_period_tag>
+class xorshift_engine;
+
+template<typename uintT, uintT a, uintT b, uintT c>
+class xorshift_engine<uintT, a, b, c, xorshift_period_tag::p32>
+{
+  public:
+    static_assert(std::is_unsigned<uintT>::value,
+                  "xorshift engine require unsigned integer type");
+
+    using uint_type = uintT;
+    using result_type = uint_type;
+    constexpr static uint_type A = a;
+    constexpr static uint_type B = b;
+    constexpr static uint_type C = c;
+
+  public:
+
+    xorshift_engine() : x_(2463534242){}
+    ~xorshift_engine() = default;
+
+    template<typename T>
+    xorshift_engine(T s) : x_(static_cast<uint_type>(s)){}
+
+    xorshift_engine(xorshift_engine const& rhs) = default;
+    xorshift_engine(xorshift_engine&&      rhs) = default;
+
+    result_type operator()() noexcept
+    {
+        x_ ^= (x_ << A);
+        x_ ^= (x_ >> B);
+        return x_ ^= (x_ << C);
+    }
+    void discard(std::size_t z) noexcept
+    {
+        while(z-- != 0){this->operator()();}
+        return;
+    }
+
+    static constexpr
+    result_type max(){return std::numeric_limits<result_type>::max();}
+    static constexpr
+    result_type min(){return std::numeric_limits<result_type>::min();}
+
+  private:
+    result_type x_;
+};
+
+template<typename uintT, uintT a, uintT b, uintT c>
+class xorshift_engine<uintT, a, b, c, xorshift_period_tag::p64>
+{
+  public:
+    static_assert(std::is_unsigned<uintT>::value,
+                  "xorshift engine require unsigned integer type");
+
+    using uint_type   = uintT;
+    using result_type = uint_type;
+    constexpr static uint_type A = a;
+    constexpr static uint_type B = b;
+    constexpr static uint_type C = c;
+
+  public:
+
+    xorshift_engine() : x_(88172645463325252){}
+    ~xorshift_engine() = default;
+
+    template<typename T>
+    xorshift_engine(T s) : x_(static_cast<uint_type>(s)){}
+
+    xorshift_engine(xorshift_engine const& rhs) = default;
+    xorshift_engine(xorshift_engine&&      rhs) = default;
+
+    result_type operator()() noexcept
+    {
+        x_ ^= (x_ << A);
+        x_ ^= (x_ >> B);
+        return x_ ^= (x_ << C);
+    }
+    void discard(std::size_t z) noexcept
+    {
+        while(z-- != 0){this->operator()();}
+        return;
+    }
+
+    static constexpr
+    result_type max(){return std::numeric_limits<result_type>::max();}
+    static constexpr
+    result_type min(){return std::numeric_limits<result_type>::min();}
+
+  private:
+    result_type x_;
+};
+
+template<typename uintT, uintT a, uintT b, uintT c>
+class xorshift_engine<uintT, a, b, c, xorshift_period_tag::p96>
+{
+  public:
+    static_assert(std::is_unsigned<uintT>::value,
+                  "xorshift engine require unsigned integer type");
+
+    using uint_type   = uintT;
+    using result_type = uint_type;
+    constexpr static uint_type A = a;
+    constexpr static uint_type B = b;
+    constexpr static uint_type C = c;
+
+  public:
+
+    xorshift_engine() : x_(123456789), y_(362436069), z_(521288629){}
+    ~xorshift_engine() = default;
+
+    //TODO ctor(sseq) & seed
+
+    xorshift_engine(xorshift_engine const& rhs) = default;
+    xorshift_engine(xorshift_engine&&      rhs) = default;
+
+    result_type operator()() noexcept
+    {
+        const result_type t = (x_ ^ (x_ << A)); x_=y_; y_=z_;
+        return z_ = (z_ ^ (z_ >> C)) ^ (t ^ (t >> B));
+    }
+    void discard(std::size_t count) noexcept
+    {
+        while(count-- != 0){this->operator()();}
+        return;
+    }
+
+    static constexpr
+    result_type max(){return std::numeric_limits<result_type>::max();}
+    static constexpr
+    result_type min(){return std::numeric_limits<result_type>::min();}
+
+  private:
+    result_type x_, y_, z_;
+};
+
+template<typename uintT, uintT a, uintT b, uintT c>
+class xorshift_engine<uintT, a, b, c, xorshift_period_tag::p128>
+{
+  public:
+    static_assert(std::is_unsigned<uintT>::value,
+                  "xorshift engine require unsigned integer type");
+
+    using uint_type   = uintT;
+    using result_type = uint_type;
+    constexpr static uint_type A = a;
+    constexpr static uint_type B = b;
+    constexpr static uint_type C = c;
+
+  public:
+
+    xorshift_engine(): x_(123456789), y_(362436069), z_(521288629), w_(88675123)
+    {}
+    ~xorshift_engine() = default;
+
+    xorshift_engine(xorshift_engine const& rhs) = default;
+    xorshift_engine(xorshift_engine&&      rhs) = default;
+
+    result_type operator()() noexcept
+    {
+        const result_type t = (x_ ^ (x_ << A)); x_ = y_; y_ = z_; z_ = w_;
+        return w_ = (w_ ^ (w_ >> C)) ^ (t ^ (t >> B));
+    }
+    void discard(std::size_t count) noexcept
+    {
+        while(count-- != 0){this->operator()();}
+        return;
+    }
+
+    static constexpr
+    result_type max(){return std::numeric_limits<result_type>::max();}
+    static constexpr
+    result_type min(){return std::numeric_limits<result_type>::min();}
+
+  private:
+    result_type x_, y_, z_, w_;
+};
+
+template<typename uintT, uintT a, uintT b, uintT c>
+class xorshift_engine<uintT, a, b, c, xorshift_period_tag::p160>
+{
+  public:
+    static_assert(std::is_unsigned<uintT>::value,
+                  "xorshift engine require unsigned integer type");
+
+    using uint_type   = uintT;
+    using result_type = uint_type;
+    constexpr static uint_type A = a;
+    constexpr static uint_type B = b;
+    constexpr static uint_type C = c;
+
+  public:
+
+    xorshift_engine()
+    : x_(123456789), y_(362436069), z_(521288629), w_(88675123), v_(5783321)
+    {}
+    ~xorshift_engine() = default;
+
+    xorshift_engine(xorshift_engine const& rhs) = default;
+    xorshift_engine(xorshift_engine&&      rhs) = default;
+
+    result_type operator()() noexcept
+    {
+        const result_type t = (x_ ^ (x_ >> A));
+        x_ = y_; y_ = z_; z_ = w_; w_ = v_;
+        return v_ = (v_ ^ (v_ >> C)) ^ (t ^ (t >> B));
+    }
+    void discard(std::size_t count) noexcept
+    {
+        while(count-- != 0){this->operator()();}
+        return;
+    }
+
+    static constexpr
+    result_type max(){return std::numeric_limits<result_type>::max();}
+    static constexpr
+    result_type min(){return std::numeric_limits<result_type>::min();}
+
+  private:
+    result_type x_, y_, z_, w_, v_;
+};
+
+typedef xorshift_engine<std::uint32_t, 13, 17, 5, xorshift_period_tag::p32>
+        xorshift32;
+typedef xorshift_engine<std::uint64_t, 13, 7, 17, xorshift_period_tag::p64>
+        xorshift64;
+typedef xorshift_engine<std::uint32_t,  3, 19, 6, xorshift_period_tag::p96>
+        xorshift96;
+typedef xorshift_engine<std::uint32_t, 11, 19, 8, xorshift_period_tag::p128>
+        xorshift128;
+typedef xorshift_engine<std::uint32_t,  2,  1, 4, xorshift_period_tag::p160>
+        xorshift160;
+
+}//namespace psd
+#endif //PSEUDO_XORSHIFT
