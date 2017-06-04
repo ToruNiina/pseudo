0e7bdcb05fab5f2fa15a7efacad4b369eb811e2e
ToruNiina <niina.toru.68u@gmail.com>
2017-06-05 00:54:06 +0900

    add ctor(seed) and seed method to xorshift engine

--- pseudo/xorshift.hpp
+++ pseudo/xorshift.hpp
@@ -11,6 +11,7 @@
 
 #include <type_traits>
 #include <limits>
+#include <array>
 #include <cstdint>
 
 namespace psd
@@ -48,8 +49,30 @@ class xorshift_engine<uintT, a, b, c, xorshift_period_tag::p32>
     xorshift_engine() : x_(2463534242){}
     ~xorshift_engine() = default;
 
-    template<typename T>
+    template<typename T, typename std::enable_if<
+        std::is_integral<T>::value, std::nullptr_t>::type = nullptr>
     xorshift_engine(T s) : x_(static_cast<uint_type>(s)){}
+
+    template<typename T, typename std::enable_if<
+        std::is_class<T>::value, std::nullptr_t>::type = nullptr>
+    xorshift_engine(T& s){this->seed(s);}
+
+    template<typename T, typename std::enable_if<
+        std::is_integral<T>::value, std::nullptr_t>::type = nullptr>
+    void seed(T s) noexcept
+    {
+        x_ = static_cast<uint_type>(s);
+    }
+
+    template<typename T, typename std::enable_if<
+        std::is_class<T>::value, std::nullptr_t>::type = nullptr>
+    void seed(T& s)
+    {// assuming T is std::seed_seq or compatible class
+        std::array<uint_type, 1> tmp;
+        s.generate(tmp.begin(), tmp.end());
+        x_ = tmp.front();
+        return;
+    }
 
     xorshift_engine(xorshift_engine const& rhs) = default;
     xorshift_engine(xorshift_engine&&      rhs) = default;
@@ -95,8 +118,29 @@ class xorshift_engine<uintT, a, b, c, xorshift_period_tag::p64>
     xorshift_engine() : x_(88172645463325252){}
     ~xorshift_engine() = default;
 
-    template<typename T>
+    template<typename T, typename std::enable_if<
+        std::is_integral<T>::value, std::nullptr_t>::type = nullptr>
     xorshift_engine(T s) : x_(static_cast<uint_type>(s)){}
+    template<typename T, typename std::enable_if<
+        std::is_class<T>::value, std::nullptr_t>::type = nullptr>
+    xorshift_engine(T& s){this->seed(s);}
+
+    template<typename T, typename std::enable_if<
+        std::is_integral<T>::value, std::nullptr_t>::type = nullptr>
+    void seed(T s) noexcept
+    {
+        x_ = static_cast<uint_type>(s);
+    }
+
+    template<typename T, typename std::enable_if<
+        std::is_class<T>::value, std::nullptr_t>::type = nullptr>
+    void seed(T& s)
+    {// assuming T is std::seed_seq or compatible class
+        std::array<std::uint32_t, 2> tmp;
+        s.generate(tmp.begin(), tmp.end());
+        x_ = (static_cast<uint_type>(tmp[0]) << 32) ^ static_cast<uint_type>(tmp[1]);
+        return;
+    }
 
     xorshift_engine(xorshift_engine const& rhs) = default;
     xorshift_engine(xorshift_engine&&      rhs) = default;
@@ -142,7 +186,26 @@ class xorshift_engine<uintT, a, b, c, xorshift_period_tag::p96>
     xorshift_engine() : x_(123456789), y_(362436069), z_(521288629){}
     ~xorshift_engine() = default;
 
-    //TODO ctor(sseq) & seed
+    template<typename T, typename std::enable_if<
+        std::is_integral<T>::value, std::nullptr_t>::type = nullptr>
+    xorshift_engine(T s)
+    : x_(static_cast<uint_type>(s)), y_(362436069), z_(521288629){}
+
+    template<typename T, typename std::enable_if<
+        std::is_class<T>::value, std::nullptr_t>::type = nullptr>
+    xorshift_engine(T& s){this->seed(s);}
+
+    template<typename T, typename std::enable_if<
+        std::is_class<T>::value, std::nullptr_t>::type = nullptr>
+    void seed(T& s)
+    {// assuming T is std::seed_seq or compatible class
+        std::array<uint_type, state_size> tmp;
+        s.generate(tmp.begin(), tmp.end());
+        x_ = tmp[0];
+        y_ = tmp[1];
+        z_ = tmp[2];
+        return;
+    }
 
     xorshift_engine(xorshift_engine const& rhs) = default;
     xorshift_engine(xorshift_engine&&      rhs) = default;
@@ -191,6 +254,35 @@ class xorshift_engine<uintT, a, b, c, xorshift_period_tag::p128>
     xorshift_engine(xorshift_engine const& rhs) = default;
     xorshift_engine(xorshift_engine&&      rhs) = default;
 
+    template<typename T, typename std::enable_if<
+        std::is_integral<T>::value, std::nullptr_t>::type = nullptr>
+    xorshift_engine(T s) : x_(static_cast<uint_type>(s)),
+        y_(362436069), z_(521288629), w_(88675123)
+    {}
+    template<typename T, typename std::enable_if<
+        std::is_class<T>::value, std::nullptr_t>::type = nullptr>
+    xorshift_engine(T& s){this->seed(s);}
+
+    template<typename T, typename std::enable_if<
+        std::is_integral<T>::value, std::nullptr_t>::type = nullptr>
+    void seed(T s) noexcept
+    {
+        x_ = static_cast<uint_type>(s);
+    }
+
+    template<typename T, typename std::enable_if<
+        std::is_class<T>::value, std::nullptr_t>::type = nullptr>
+    void seed(T& s)
+    {// assuming T is std::seed_seq or compatible class
+        std::array<uint_type, state_size> tmp;
+        s.generate(tmp.begin(), tmp.end());
+        x_ = tmp[0];
+        y_ = tmp[1];
+        z_ = tmp[2];
+        w_ = tmp[3];
+        return;
+    }
+
     result_type operator()() noexcept
     {
         const result_type t = (x_ ^ (x_ << A)); x_ = y_; y_ = z_; z_ = w_;
@@ -235,6 +327,31 @@ class xorshift_engine<uintT, a, b, c, xorshift_period_tag::p160>
 
     xorshift_engine(xorshift_engine const& rhs) = default;
     xorshift_engine(xorshift_engine&&      rhs) = default;
+
+    template<typename T, typename std::enable_if<
+        std::is_integral<T>::value, std::nullptr_t>::type = nullptr>
+    xorshift_engine(T s)
+    : x_(static_cast<uint_type>(s)), y_(362436069), z_(521288629), w_(88675123),
+      v_(5783321)
+    {}
+
+    template<typename T, typename std::enable_if<
+        std::is_class<T>::value, std::nullptr_t>::type = nullptr>
+    xorshift_engine(T& s){this->seed(s);}
+
+    template<typename T, typename std::enable_if<
+        std::is_class<T>::value, std::nullptr_t>::type = nullptr>
+    void seed(T& s)
+    {// assuming T is std::seed_seq or compatible class
+        std::array<uint_type, state_size> tmp;
+        s.generate(tmp.begin(), tmp.end());
+        x_ = tmp[0];
+        y_ = tmp[1];
+        z_ = tmp[2];
+        w_ = tmp[3];
+        v_ = tmp[4];
+        return;
+    }
 
     result_type operator()() noexcept
     {
