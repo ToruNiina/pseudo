0d9b4dfc0e021cdee30735aeb16bb4b09e14ddea
ToruNiina <niina.toru.68u@gmail.com>
2017-06-05 17:16:12 +0900

    add is_all and has_iterator traits

--- pseudo/type_traits.hpp
+++ pseudo/type_traits.hpp
@@ -18,10 +18,19 @@ std::true_type is_iterator_impl(int);
 template<typename T>
 std::false_type is_iterator_impl(long);
 
+template<typename T, class = typename T::iterator,
+         decltype(std::declval<T>().begin())* = nullptr,
+         decltype(std::declval<T>().end())* = nullptr>
+std::true_type has_iterator_impl(int);
+template<typename T>
+std::false_type has_iterator_impl(long);
+
 } // detail
 
 template<typename T>
 struct is_iterator : decltype(detail::is_iterator_impl<T>(0)){};
+template<typename T>
+struct has_iterator : decltype(detail::has_iterator_impl<T>(0)){};
 
 template<typename ...>
 struct meta_or;
@@ -33,7 +42,7 @@ template<typename T1, typename T2>
 struct meta_or<T1, T2> : public std::conditional<T1::value, T1, T2>::type {};
 template<typename T1, typename T2, typename T3, typename ... Ts>
 struct meta_or<T1, T2, T3, Ts...>
-: public std::conditional<T1::value, T1, _or_<T2, T3, Ts...>>::type {};
+: public std::conditional<T1::value, T1, meta_or<T2, T3, Ts...>>::type {};
 
 template<typename ...>
 struct meta_and;
@@ -49,6 +58,15 @@ struct meta_and<T1, T2, T3, Ts...>
 
 template<typename T>
 struct meta_not : public std::integral_constant<bool, !T::value>{};
+
+template<template<typename T> class opT, typename ... Ts>
+struct is_all;
+template<template<typename T> class opT, typename T>
+struct is_all<opT, T> : opT<T>{};
+template<template<typename T> class opT, typename T, typename ...Ts>
+struct is_all<opT, T, Ts...> : meta_and<opT<T>, is_all<opT, Ts...>>{};
+
+
 
 }//psd
 #endif /* PSEUDO_TYPE_TRAITS */
