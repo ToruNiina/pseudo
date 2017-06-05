7189c86b55fabcce9b77e19d88ae4de8a6e41a05
ToruNiina <niina.toru.68u@gmail.com>
2017-06-05 15:56:47 +0900

    add is_iterator trait

--- pseudo/type_traits.hpp
+++ pseudo/type_traits.hpp
@@ -1,44 +1,27 @@
 #ifndef PSEUDO_TYPE_TRAITS
 #define PSEUDO_TYPE_TRAITS
-
-#if __cplusplus >= 201103L
 #include <type_traits>
-
-// forward decl (not good?)
-namespace std
-{
-template<class T> class reference_wrapper;
-}//std
+#include <iterator>
 
 namespace psd
 {
 
-template<typename T>
-struct strip_reference_wrapper
+namespace detail
 {
-    typedef T type;
-};
+
+template<typename T, class = typename std::iterator_traits<T>::value_type,
+         class = typename std::iterator_traits<T>::pointer,
+         class = typename std::iterator_traits<T>::reference,
+         class = typename std::iterator_traits<T>::difference_type,
+         class = typename std::iterator_traits<T>::iterator_category>
+std::true_type is_iterator_impl(int);
+template<typename T>
+std::false_type is_iterator_impl(long);
+
+} // detail
 
 template<typename T>
-struct strip_reference_wrapper<std::reference_wrapper<T>>
-{
-    typedef T& type;
-};
-
-template<typename T>
-using strip_ref_t = typename strip_reference_wrapper<T>::type;
-
-template<typename T>
-struct decay_and_strip
-{
-    typedef typename std::decay<T>::type decayed_type;
-    typedef strip_ref_t<decayed_type> type;
-};
-
-template<typename T>
-using decay_and_strip_t = typename decay_and_strip<T>::type;
+struct is_iterator : decltype(detail::is_iterator_impl<T>(0)){};
 
 }//psd
-
-#endif // c++11
 #endif /* PSEUDO_TYPE_TRAITS */
